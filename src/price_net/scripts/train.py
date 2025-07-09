7d523a4129313c5b8fafb2cd58fe02adaf058ba6
Spencer Young <spencermyoung513@gmail.com>
2025-07-09 12:03:00 -0600

    Bugfix with enum value

--- src/price_net/scripts/train.py
+++ src/price_net/scripts/train.py
@@ -52,7 +52,7 @@ def train(config: TrainingConfig):
         logger=logger,
         callbacks=[best_ckpt_callback, last_ckpt_callback],
         enable_model_summary=False,
-        precision=config.precision,
+        precision=config.precision.value,
     )
     model = PriceAssociatorLightningModule(
         num_epochs=config.num_epochs,
