7f25e80c236f1589c3ec46d4e22c158a6ecffd1a
stocktonjenkins <stockton.jenkins@deliciousai.com>
2025-07-15 09:35:36 -0500

    fix: use class variable for temperature

--- src/price_net/scripts/build_extraction_dataset.py
+++ src/price_net/scripts/build_extraction_dataset.py
@@ -5,14 +5,17 @@ import pandas as pd
 from PIL import Image
 from tqdm import tqdm
 
-PRICE_BOXES_FNAME = 'price_boxes.csv'
+PRICE_BOXES_FNAME = "price_boxes.csv"
 ATTRIBUTION_SET_ID_COL = "attributionset_id"
 PRICE_BBOX_ID_COL = "price_bbox_id"
 SCENE_IMG_DIR = "images"
 PRICE_IMG_DIR = "price-images"
 IMG_EXT = "jpg"
 
-def crop_with_relative_bbox(image: Image.Image, bbox: tuple[float, float, float, float]) -> Image.Image:
+
+def crop_with_relative_bbox(
+    image: Image.Image, bbox: tuple[float, float, float, float]
+) -> Image.Image:
     """
     Crop an image using relative bounding box coordinates.
 
@@ -45,8 +48,8 @@ def save_cropped_image(image: Image.Image, output_path: Path) -> None:
     output_path.parent.mkdir(parents=True, exist_ok=True)
     image.save(output_path.with_suffix(".jpg"), format="JPEG")
 
-def main(dataset_path: Path):
 
+def main(dataset_path: Path):
     price_boxes_path = dataset_path / PRICE_BOXES_FNAME
     price_boxes = pd.read_csv(price_boxes_path)
 
@@ -56,33 +59,36 @@ def main(dataset_path: Path):
     print(f"Number of images: {len(num_images)}")
     print(f"Number of price boxes: {len(num_price_tags)}")
 
-    for attribution_id, vals in tqdm(price_boxes.groupby(ATTRIBUTION_SET_ID_COL), total=len(num_images)):
-        scene_img = Image.open(dataset_path / SCENE_IMG_DIR / f"{attribution_id}.{IMG_EXT}")
+    for attribution_id, vals in tqdm(
+        price_boxes.groupby(ATTRIBUTION_SET_ID_COL), total=len(num_images)
+    ):
+        scene_img = Image.open(
+            dataset_path / SCENE_IMG_DIR / f"{attribution_id}.{IMG_EXT}"
+        )
 
         for _, tag_instance in vals.iterrows():
             price_bbox_id = tag_instance[PRICE_BBOX_ID_COL]
             price_tag_img = crop_with_relative_bbox(
                 scene_img,
                 (
-                    tag_instance['min_x'],
-                    tag_instance['min_y'],
-                    tag_instance['max_x'],
-                    tag_instance['max_y']
+                    tag_instance["min_x"],
+                    tag_instance["min_y"],
+                    tag_instance["max_x"],
+                    tag_instance["max_y"],
                 ),
             )
 
             save_cropped_image(
                 price_tag_img,
-                dataset_path / PRICE_IMG_DIR / f"{price_bbox_id}.{IMG_EXT}"
+                dataset_path / PRICE_IMG_DIR / f"{price_bbox_id}.{IMG_EXT}",
             )
 
 
-
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset-path", type=Path, help="Path to the dataset (train or test)")
+    parser.add_argument(
+        "--dataset-path", type=Path, help="Path to the dataset (train or test)"
+    )
     args = parser.parse_args()
     dataset_path = Path(args.dataset_path)
     main(dataset_path)
-
